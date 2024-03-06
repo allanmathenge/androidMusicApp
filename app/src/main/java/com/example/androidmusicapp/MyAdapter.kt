d1b5dc9930999b36ee5950661698169f3a98dd16
unknown <allanmathenge2.com>
2024-03-06 20:39:16 +0300

    adding to MyAdapter

--- app/src/main/java/com/example/androidmusicapp/MyAdapter.kt
+++ app/src/main/java/com/example/androidmusicapp/MyAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.androidmusicapp
 
 import android.app.Activity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
@@ -8,32 +9,38 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-// Connects the User Interfaces with with data sources
-class MyAdapter(val context: Activity, private val dataList: List<Data>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        // Create the view in case the layout manager fail to create view for the data
-
-        val image: ImageView
-        val title: TextView
-        val play: ImageButton
-        val pause: ImageButton
-
-    }
+class MyAdapter(private val context: Activity, private val dataList: List<Data>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        // Creates the views holder when the layout manager fails to do so
 
-
+        val itemView = LayoutInflater.from(context).inflate(R.layout.each_item, parent, false)
+        return MyViewHolder(itemView)
     }
-
 
     override fun getItemCount(): Int {
         // Returns the total number of items in the data set held by the adapter
         return dataList.size
     }
 
-
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // Populate the data into the view
 
+    }
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        // Holds and initiates all the views we have
+
+        private val image: ImageView
+        private val title: TextView
+        private val play: ImageButton
+        private val pause: ImageButton
+
+        init {
+            image = itemView.findViewById(R.id.musicImage)
+            title = itemView.findViewById(R.id.musicTitle)
+            play = itemView.findViewById(R.id.btnPlay)
+            pause = itemView.findViewById(R.id.btnPause)
+        }
     }
 }
